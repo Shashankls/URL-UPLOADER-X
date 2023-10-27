ce81d259cc689574a12fe989fd5ba6718ab9ea9e
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-28 00:03:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6646979040:AAEFeP64V3U-NFcadq2k68GML8g6M0B5f4Q")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID","16621664"))
+    API_HASH = os.environ.get("API_HASH","8b283f2943729318995738b5963f0bcc")
     # Get these values from my.telegram.org
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6488346050").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
@@ -14,18 +16,15 @@ class Config(object):
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
-    CHUNK_SIZE = int(128)
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = ""
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
-    # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "UPLOADER-X-BOT"
-    # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    MAX_RESULTS = "50"
+    # watermark file
+    DEF_WATER_MARK_FILE = ""
